e853b56b30673f2823d38eafe5d8f8855ca048a3
annt <annt@gft.com>
2022-11-21 12:28:38 +0000

    done with HashMap

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,43 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public Solution(){
+
+    }
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        for(int i = 0;i<magazine.length();i++){
+
+            char character = magazine.charAt(i);
+
+            if(map.containsKey(character)){
+                map.put(character,map.get(character)+1);
+            }else{
+
+                map.put(character,1);
+            }
+        }
+        for(int i = 0;i<ransomNote.length();i++){
+
+            char character = ransomNote.charAt(i);
+
+            if(!map.containsKey(character) || map.get(character) <= 0){
+
+                return false;
+
+            }else{
+
+                map.put(character,map.get(character)-1);
+            }
+        }
+
+        return true;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,7 +1,53 @@
-import org.testng.annotations.Test;
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class Solution {
+public class SolutionTest {
+
+    Solution solution;
+
+    String ransomNote;
+
+    String magazine;
+
+    @BeforeAll
+    void setup(){
+        solution = new Solution();
+    }
+    @Test
+    public void WithOneLetterMakeAnotherLetter(){
+        ransomNote = "a";
+        magazine = "a";
+
+        assertTrue(solution.canConstruct(ransomNote,magazine));
+    }
 
     @Test
+    public void WithOneLetterCannotMakeAnotherLetter(){
+        ransomNote = "a";
+        magazine = "b";
 
+        assertFalse(solution.canConstruct(ransomNote,magazine));
+    }
+
+    @Test
+    public void WithMoreThanOneLetterMakeAnotherLetterWord(){
+        ransomNote = "abc";
+        magazine = "abacus";
+
+        assertTrue(solution.canConstruct(ransomNote,magazine));
+    }
+
+    @Test
+    public void WithMoreThanOneLetterCannotMakeAnotherWord(){
+        ransomNote = "abc";
+        magazine = "ertab";
+
+        assertFalse(solution.canConstruct(ransomNote,magazine));
+    }
 }
